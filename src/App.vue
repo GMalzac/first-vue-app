06c9978f7fcd484de0de5efad41e7e6e8f3aff11
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-04-01 18:57:01 +0100

    App init

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ hello }}</h1>
   </div>
 </template>
 
@@ -10,6 +11,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      hello: "Hello world!!!"
+    };
+  },
   components: {
     HelloWorld
   }
